135be32e698fe3e6a461c57092a81c35f89aa27a
ZelinMa557 <3388706467@qq.com>
2024-06-22 19:03:11 +0800

    fix some bug of rabin cdc
    Signed-off-by: ZelinMa557 <3388706467@qq.com>

--- chunk/rabin_cdc.cpp
+++ chunk/rabin_cdc.cpp
@@ -7,8 +7,8 @@
 #include <glog/logging.h>
 namespace Delta {
 namespace {
-constexpr uint64_t min_chunk_size = 8192 - 4096;
-constexpr uint64_t max_chunk_size = 8192 + 4096;
+constexpr uint64_t min_chunk_size = 8192 - 2048;
+constexpr uint64_t max_chunk_size = 8192 + 2048;
 // the lower 13 bits are 1.
 // see https://pdos.csail.mit.edu/papers/lbfs:sosp01/lbfs.pdf
 constexpr uint64_t rabin_stop_mask = (1UL << 14) - 1;
@@ -16,32 +16,35 @@ constexpr uint64_t rabin_stop_mask = (1UL << 14) - 1;
 constexpr uint64_t polynomial = 0x3DA3358B4DC173LL;
 constexpr uint64_t polynomial_degree = 53;
 constexpr uint64_t polynomial_shift = polynomial_degree - 8;
+
+int deg(uint64_t p) {
+  uint64_t mask = 0x8000000000000000LL;
+  for (int i = 0; i < 64; i++) {
+    if ((mask & p) > 0) {
+      return 63 - i;
+    }
+    mask >>= 1;
+  }
+  return -1;
+}
+
+uint64_t mod(uint64_t x, uint64_t p) {
+  while (deg(x) >= deg(p)) {
+    uint64_t shift = deg(x) - deg(p);
+    x = x ^ (p << shift);
+  }
+  return x;
+}
+
+uint64_t append_byte(uint64_t hash, uint8_t b, uint64_t pol) {
+  hash <<= 8;
+  hash |= (uint64_t)b;
+  return mod(hash, pol);
+}
+
 } // namespace
 
 RabinCDC::RabinCDC() {
-  auto deg = [&](uint64_t p) -> uint64_t {
-    uint64_t mask = 0x8000000000000000LL;
-    for (int i = 0; i < 64; i++) {
-      if ((mask & p) > 0) {
-        return 63 - i;
-      }
-      mask >>= 1;
-    }
-    return -1;
-  };
-  auto mod = [&](uint64_t x, uint64_t p) -> uint64_t {
-    while (deg(x) >= deg(p)) {
-      uint64_t shift = deg(x) - deg(p);
-      x = x ^ (p << shift);
-    }
-    return x;
-  };
-  auto append_byte = [&](uint64_t hash, uint8_t b, uint64_t pol) -> uint64_t {
-    hash <<= 8;
-    hash |= (uint64_t)b;
-    return mod(hash, pol);
-  };
-
   // calculate table for sliding out bytes. The byte to slide out is used as
   // the index for the table, the value contains the following:
   // out_table[b] = Hash(b || 0 ||        ...        || 0)
@@ -53,9 +56,9 @@ RabinCDC::RabinCDC() {
   //  = H(    0     || b_1 || ...     || b_w)
   //
   // Afterwards a new byte can be shifted in.
+  LOG(INFO) << "start calc rabin table...";
   for (int b = 0; b < 256; b++) {
     uint64_t hash = 0;
-
     hash = append_byte(hash, (uint8_t)b, polynomial);
     for (int i = 0; i < WINSIZE - 1; i++) {
       hash = append_byte(hash, 0, polynomial);
@@ -109,7 +112,8 @@ bool RabinCDC::ReinitWithFile(std::string file_name) {
   }
   this->file_read_ptr = this->file.get_mapped_addr();
   this->remaining_file_len = this->file.get_maped_len();
-  LOG(INFO) << "RabinCDC inited with file: " << file_name << " length: " << remaining_file_len;
+  LOG(INFO) << "RabinCDC inited with file: " << file_name
+            << " length: " << remaining_file_len;
   return true;
 }
 
@@ -120,11 +124,10 @@ std::shared_ptr<Chunk> RabinCDC::GetNextChunk() {
     auto chunk_size = remaining_file_len;
     remaining_file_len = 0;
     LOG(INFO) << "RabinCDC split file " << this->file.get_file_name()
-               << "chunk offset: "
-               << (uint64_t)(file_read_ptr - file.get_mapped_addr())
-               << " length: " << remaining_file_len;
-    return Chunk::FromMemoryRef(file_read_ptr, chunk_size,
-                                get_next_chunk_id());
+              << " chunk offset: "
+              << (uint64_t)(file_read_ptr - file.get_mapped_addr())
+              << " length: " << remaining_file_len;
+    return Chunk::FromMemoryRef(file_read_ptr, chunk_size, get_next_chunk_id());
   }
   uint8_t *read_start = file_read_ptr;
   for (uint64_t chunk_size = 1; chunk_size <= remaining_file_len;
@@ -137,9 +140,9 @@ std::shared_ptr<Chunk> RabinCDC::GetNextChunk() {
       rabin_reset();
       remaining_file_len -= chunk_size;
       LOG(INFO) << "RabinCDC split file " << this->file.get_file_name()
-                 << "chunk offset: "
-                 << (uint64_t)(read_start - file.get_mapped_addr())
-                 << " length: " << chunk_size;
+                << " chunk offset: "
+                << (uint64_t)(read_start - file.get_mapped_addr())
+                << " length: " << chunk_size;
       return Chunk::FromMemoryRef(read_start, chunk_size, get_next_chunk_id());
     }
   }
