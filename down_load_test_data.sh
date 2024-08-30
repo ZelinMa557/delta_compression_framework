0e4db2b8c72409ee345b8dac327ba584ecf1119e
ZelinMa557 <3388706467@qq.com>
2024-08-31 00:55:31 +0800

    updates
    Signed-off-by: ZelinMa557 <3388706467@qq.com>

--- down_load_test_data.sh
+++ down_load_test_data.sh
@@ -4,58 +4,46 @@ create_if_not_exist() {
         mkdir -p $1
     fi
 }
-# create_if_not_exist test_data/glib
+create_if_not_exist test_data/glib
 
-# cd test_data/glib
-# for i in {61..80}
-# do
-#     wget https://download.gnome.org/sources/glib/2.$i/glib-2.$i.0.tar.xz
-# done
+cd test_data/glib
+for i in {61..80}
+do
+    wget https://download.gnome.org/sources/glib/2.$i/glib-2.$i.0.tar.xz
+done
 
 
-# xz -d *.xz
-# rm -rf *.xz
+xz -d *.xz
+rm -rf *.xz
 
-# cd ../..
-# create_if_not_exist test_data/linux
+cd ../..
+create_if_not_exist test_data/linux
 
-# cd test_data/linux
-# for i in {1..5}
-# do
-#     for (( j=1; j<=10; j+=3))
-#     do
-#         wget https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.$i.$j.tar.xz
-#     done
-# done
-# xz -d *.xz
-# rm -rf *.xz
-# cd ../..
+cd test_data/linux
+for i in {1..5}
+do
+    for (( j=1; j<=10; j+=3))
+    do
+        wget https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.$i.$j.tar.xz
+    done
+done
+xz -d *.xz
+rm -rf *.xz
+cd ../..
 
-# create_if_not_exist test_data/vscodes
+create_if_not_exist test_data/postgres
+cd test_data/postgres
+postgre_versions=(12.0 12.4 12.8 12.12 12.16
+                  13.0 13.4 13.8 13.12 13.16
+                  14.0 14.4 14.8 14.12 14.13
+                  15.0 15.4 15.8 16.0 16.4)
+for postgre_version in "${postgre_versions[@]}"
+do
+  wget https://ftp.postgresql.org/pub/source/v$postgre_version/postgresql-$postgre_version.tar.gz
+  gzip -d postgresql-$postgre_version.tar.gz
+done
 
-# cd test_data/vscodes
-# https://github.com/microsoft/vscode/archive/refs/tags/1.80.0.tar.gz
-# for (( i=30; i<=90; i+=2 ))
-# do
-#   wget https://github.com/microsoft/vscode/archive/refs/tags/1.$i.0.tar.gz
-#   gzip -d 1.$i.0.tar.gz
-# done
-
-# cd ../..
-
-# create_if_not_exist test_data/postgres
-# cd test_data/postgres
-# postgre_versions=(12.0 12.4 12.8 12.12 12.16
-#                   13.0 13.4 13.8 13.12 13.16
-#                   14.0 14.4 14.8 14.12 14.13
-#                   15.0 15.4 15.8 16.0 16.4)
-# for postgre_version in "${postgre_versions[@]}"
-# do
-#   wget https://ftp.postgresql.org/pub/source/v$postgre_version/postgresql-$postgre_version.tar.gz
-#   gzip -d postgresql-$postgre_version.tar.gz
-# done
-
-# cd ../..
+cd ../..
 
 create_if_not_exist test_data/gcc
 cd test_data/gcc
--- readme.md
+++ readme.md
@@ -1,13 +1,13 @@
-# 依赖项
-1. 请确保系统中包含`gflags` `glog` `gtest` 库。
+本项目是一个差值压缩原型系统，实现了N-Transform, Finesse, Odess, Palantir等多种差值压缩方法。该系统将差值压缩流程分为分块、去重、特征计算、相似块检测、差值编码与数据块写入五个阶段，每个阶段之间采用流水线并行。
 
 # 测试数据
-执行以下代码，获取19个不同版本的glib源码的tar包:
+执行以下代码，获取20个不同版本的glib源码的tar包、20个不同版本的linux源码的tar包、20个不同版本的postgresql源码的tar包、8个不同版本的gcc源码的tar包:
 ```
 bash down_load_test_data.sh
 ```
 
 # 编译运行
+请确保系统中包含`gflags` `glog` `gtest` 库。  
 常规的cmake项目构建方式：
 ```
 mkdir build
